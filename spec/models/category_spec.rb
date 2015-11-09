c358510244a97b692f7b0927930fe3c462e54868
long.yang <long.yang@starlight-sms.com>
2015-11-09 13:43:20 +0800

    Use shoulda_matchers

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,21 +1,7 @@
 require "spec_helper"
 
 describe Category do
-  let(:category) { Category.new(name: "TV Commedies") }
-  let(:video_1)  { Video.create(title: "video_1", description: "some details") }
-  let(:video_2)  { Video.create(title: "video_2", description: "some details") }
-
-  context "When create a new category," do
-    it "can save itself" do
-      category.save
-      expect(Category.last).to eq(category)
-    end
-
-    it "can have many videos" do
-      [video_1, video_2].each { |video| video.category = category; video.save }
-      category.save
-      expect(category.videos).to eq([video_1, video_2])
-    end
-
+  it "can have many videos" do
+    should have_many(:videos)
   end
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,44 +1,17 @@
 require "spec_helper"
 
 describe Video do
-  let(:new_category) { Category.create(name: "TV Commedies") }
-
-  it "can save self" do
-    title = "new_video"
-    description = "new_video_description"
-    small_cover_url =  "small_cover_url_video"
-    large_cover_url =  "large_cover_url_video"
-
-    video = Video.new(title: title,
-                      description: description,
-                      small_cover_url: small_cover_url,
-                      large_cover_url: large_cover_url,
-                     )
-    video.save
-    last_video = Video.last
-    expect(last_video.title).to eq(title)
-    expect(last_video.description).to eq(description)
-    expect(last_video.small_cover_url).to eq(small_cover_url)
-    expect(last_video.large_cover_url).to eq(large_cover_url)
-  end
 
   it "belongs to category" do
-    video = Video.create(title:"new_video", category: new_category)
-    expect(video.category).to eq(new_category)
+    should belong_to(:category)
   end
 
-  it "if have title and description, should be valid" do
-    video = Video.create(title:"new video", description: "some details")
-    expect(Video.first).to eq(video)
+  it "have a title" do
+    should validate_presence_of(:title)
   end
 
-  it 'it has no either title or description ' do
-    video_1 = Video.create(title:"new video")
-    video_2 = Video.create(description: "some details")
-    video_1.valid?
-    video_2.valid?
-    expect(video_1.errors[:description].size).to eq(1)
-    expect(video_2.errors[:title].size).to eq(1)
+  it "have description" do
+    should validate_presence_of(:description)
   end
 
 end
