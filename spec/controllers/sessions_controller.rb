b2501012d3d988e3eeef9fe9ae6bfb949963efc7
long.yang <long.yang@starlight-sms.com>
2015-12-17 13:57:30 +0800

    user & session controller test

--- spec/controllers/sessions_controller.rb
+++ spec/controllers/sessions_controller.rb
@@ -29,43 +29,59 @@ describe SessionsController do
   describe "POST create" do
     context "both email and password are right" do
       let(:user)  { Fabricate(:user) }
+      before do
+        post :create, email: user.email, password: user.password
+      end
 
       it "user can sign in" do
-        post :create, email: user.email, password: user.password
         expect(session[:user_id]).to eq(user.id)
       end
+
       it "redirect to vidoes page" do
-        post :create, email: user.email, password: user.password
         expect(response).to redirect_to videos_path
+      end
+
+      it "sets the notice" do
+        expect(flash[:notice]).not_to be_blank
       end
     end
 
     context "email or password is wrong" do
       let(:user)  { Fabricate(:user) }
-      it "user cannot sign in" do
+      before do
         post :create, email: user.email, password: user.password.concat("passowrd")
+      end
+      it "user cannot sign in" do
         expect(session[:user_id]).to be nil
       end
 
       it "render the new template" do
-        post :create, email: user.email, password: user.password.concat("passowrd")
         expect(response).to render_template :new
+      end
+
+      it "sets the error message" do
+        expect(flash[:danger]).not_to be_blank
       end
     end
   end
 
   describe "POST destroy" do
     let(:user)  { Fabricate(:user) }
-    before { session[:user_id] = user.id }
+    before do
+      session[:user_id] = user.id
+      post :destroy, user: user.id
+    end
 
     it "redirect to front page" do
-      post :destroy, user: user.id
       expect(response).to redirect_to sign_in_path
     end
 
-    it "no authenticated user" do
-      post :destroy, user: user.id
+    it "clear the session" do
       expect(session[:user_id]).to be nil
+    end
+
+    it "sets the notice" do
+      expect(flash[:notice]).not_to be_blank
     end
   end
 end
