b53ef088f69e0b39918da485b7489f53ce6e24f0
long.yang <long.yang@starlight-sms.com>
2016-04-14 14:42:25 +0800

    prepare deploy to heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,13 +18,14 @@ Myflix::Application.configure do
 
   config.active_support.deprecation = :notify
 
-  config.action_mailer.smtp_settings = {
-    address: "smtp.qq.com",
-    port: 25,
-    domain: "qq.com",
-    authentication: "login",
-    enable_starttls_auto: true,
-    user_name: ENV["MAIL_USERNAME"],
-    password: ENV["MAIL_PASSWORD"]
+  ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'yourapp.heroku.com',
+    :authentication => :plain,
   }
+  ActionMailer::Base.delivery_method = :smtp
+
 end
