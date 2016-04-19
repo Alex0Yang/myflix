29ac7685cd5232f2d9547d63ffa9365b4ca26adf
long.yang <long.yang@starlight-sms.com>
2016-04-19 19:59:28 +0800

    add default from_to for mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,16 +1,18 @@
 class UserMailer < ActionMailer::Base
+  default from: ENV["MAIL_USERNAME"] || 'test@test.com'
+
   def welcome_on_register(user_id)
     @user = User.find(user_id)
-    mail from: ENV["MAIL_USERNAME"], to: @user.email, subject: "Welcome to Myflix"
+    mail  to: @user.email, subject: "Welcome to Myflix"
   end
 
   def reset_password(user_id)
     @user = User.find(user_id)
-    mail from: ENV["MAIL_USERNAME"], to: @user.email, subject: "Reset password email"
+    mail  to: @user.email, subject: "Reset password email"
   end
 
   def invite_friend(invitation_id)
     @invitation = Invitation.find(invitation_id)
-    mail from: ENV["MAIL_USERNAME"], to: @invitation.friend_email, subject: "Invitation to join MyFlix"
+    mail  to: @invitation.friend_email, subject: "Invitation to join MyFlix"
   end
 end
