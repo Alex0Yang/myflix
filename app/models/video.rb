1fb43246c490c9d7eeb4cc69847b4a917b1555df
long.yang <long.yang@starlight-sms.com>
2016-05-17 19:39:00 +0800

    Assignment: Search Video by Title and Description

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,6 +1,12 @@
 class Video < ActiveRecord::Base
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
+
+  index_name ["myflix", Rails.env].join('_')
+
   mount_uploader :large_cover, LargeCoverUploader
   mount_uploader :small_cover, SmallCoverUploader
+
   belongs_to :category
   has_many :comments, -> { order('created_at desc') }
   validates :title, presence: true
@@ -20,5 +26,21 @@ class Video < ActiveRecord::Base
 
   def rating
     comments.average(:rate).round(1) if comments.average(:rate)
+  end
+
+  def as_indexed_json(options={})
+    as_json(only: [:title, :description])
+  end
+
+  def self.search(query)
+    __elasticsearch__.search(
+      query: {
+        multi_match: {
+          query: query,
+          operator: "and",
+          fields: [:title, :description]
+        }
+      }
+    )
   end
 end
