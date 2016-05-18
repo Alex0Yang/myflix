434f877cfc2ef7da4b496c6b42ac553406dae8d9
long.yang <long.yang@starlight-sms.com>
2016-05-19 07:05:37 +0800

    my solution for Assignment: Filter with Average Rating

--- app/models/video.rb
+++ app/models/video.rb
@@ -17,34 +17,37 @@ class Video < ActiveRecord::Base
     where("lower(title) like ?", "%#{search_term.downcase}%").order("created_at DESC")
   end
 
-  def average_rate
-    return 'none' unless comments.present?
-    total = 0
-    comments.reload.each { |c| total += c.rate }
-    ( total * 1.0 / comments.count ).round(1)
-  end
-
   def rating
     comments.average(:rate).round(1) if comments.average(:rate)
   end
 
   def as_indexed_json(options={})
     as_json(include: { comments: { only: :content} },
-      only: [:title, :description])
+      only: [:title, :description], methods: :rating)
   end
 
   def self.search(query, options={})
     fields = ["title^100", "description^50"]
-    fields << "comments.content" if options[:comments]
+    fields << "comments.content" if options[:comments].present?
+    filter = { range: { rating: {} }}
+    filter[:range][:rating].merge!({gte: options[:rating_from]}) if options[:rating_from]
+    filter[:range][:rating].merge!({lt: options[:rating_to]}) if options[:rating_to]
     search_term = {
-      query: {
-        multi_match: {
-          query: query,
-          operator: "and",
-          fields: fields
+        query: {
+          filtered:{
+            query: {
+              multi_match: {
+                query: query,
+                operator: "and",
+                fields: fields
+              }
+            }
+          }
         }
-      }
     }
+
+    search_term[:query][:filtered].merge!({filter: filter}) if options[:rating_to] || options[:rating_from]
+
     __elasticsearch__.search(search_term)
   end
 end
